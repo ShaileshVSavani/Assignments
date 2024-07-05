e9bc11beeecbaabfcf2a600b3c7f7935c484caee
ShaileshVSavani <svs.rnw23@gmail.com>
2024-07-05 11:30:12 +0530

    1

--- pr-2/src/App.jsx
+++ pr-2/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [inputMinutes, setInputMinutes] = useState('');
@@ -8,16 +9,16 @@ function App() {
   const [seconds, setSeconds] = useState(0);
   const [start, setStart] = useState(false);
 
-  const handleInputChange = (e) => {
+  const handleInput = (e) => {
     const value = e.target.value;
     setInputMinutes(value);
 
-    if (!isNaN(value) && value !== '') {
+    if (!isNaN(value) && value !== '' && value > 0) {
       const totalSeconds = parseInt(value) * 60;
       setHours(Math.floor(totalSeconds / 3600));
       setMinutes(Math.floor((totalSeconds % 3600) / 60));
       setSeconds(totalSeconds % 60);
-      setStart(true);
+      // setStart(true);
     } else {
       setStart(false);
       setHours(0);
@@ -55,15 +56,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Stopwatch</h1>
-      <h2>
-        Minutes:
-        <input type="number" value={inputMinutes} onChange={handleInputChange} />
+      <h1>Timer</h1>
+      <h2> 
+        <input type="number" placeholder='Enter Minutes' value={inputMinutes} onChange={handleInput} />
+        {/* <input type="number"  onChange={handleInputChange} /> */}
       </h2>
-      <div>
-        <h2>Stopwatch Time:</h2>
+    
         <h3>{hours} Hours : {minutes} Minutes : {seconds} Seconds</h3>
-      </div>
+        <button className={`btn ${start ? 'btn-danger' : 'btn-success'} , me-2`} onClick={() => setStart(!start)}>
+          {start? 'Stop' : 'Start'}
+        </button>
+        <button className="btn btn-warning" onClick={() => { setStart(false); setHours(0); setMinutes(0); setSeconds(0); setInputMinutes(''); }}>Reset</button>
+    
     </div>
   );
 }
