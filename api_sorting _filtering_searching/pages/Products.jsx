d9b7f540fa61ecd45799a23bc8a2ecebea7e00be
ShaileshVSavani <svs.rnw23@gmail.com>
2024-08-07 15:46:19 +0530

    1

--- api_sorting _filtering_searching/pages/Products.jsx
+++ api_sorting _filtering_searching/pages/Products.jsx
@@ -1,60 +1,159 @@
+
+// import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import { Link, useSearchParams } from 'react-router-dom'
+// import FilteredData from './FilteredData'
+
+// const ProductData = () => {
+//   const [products, setProducts] = useState([])
+//   const [query, setQuery] = useSearchParams()
+
+//   const getData = async () => {
+//     let res = await axios.get('https://fakestoreapi.com/products')
+//     setProducts(res.data)
+//     console.log(res.data)
+//   }
+
+//   useEffect(() => {
+//     getData()
+//   }, [])
+
+//   useEffect(() => {
+//     let sort = query.get('sort')
+//     let category = query.get('category')
+//     let search = query.get('search')
+//     console.log(sort, category, search)
+
+//     if (!sort && !category) {
+//       getData()
+//       return
+//     }
+
+//     let filteredProducts = [...products]
+
+//     if (category) {
+//       filteredProducts = filteredProducts.filter((item) => item.category === category)
+//     }
+
+//     if (sort === "LTH") {
+//       filteredProducts = filteredProducts.sort((a, b) => a.price - b.price)
+//     }
+//     else {
+//       filteredProducts = filteredProducts.sort((a, b) => b.price - a.price)
+//       }
+//       if (search) {
+//         filteredProducts = filteredProducts.filter((item) =>
+//           item.title.toLowerCase().includes(search.toLowerCase())
+//         )
+//       }
+
+
+//     setProducts(filteredProducts)
+//   }, [query])
+
+//   return (
+//     <div>
+//       <FilteredData />
+//       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 shadow gap-5 mt-5'>
+//         {products.map((item) => (
+//           <Link key={item.id} to={`/product/${item.id}`}>
+//             <div className="border p-4 rounded-lg shadow transition-transform duration-500 ease-in-out hover:shadow-lg hover:scale-105 flex flex-col h-full">
+//               <div className="flex justify-center items-center h-32 flex-shrink-0">
+//                 <img
+//                   src={item.image}
+//                   alt={item.title}
+//                   className="max-h-full max-w-full object-contain hover:scale-105 ease-in-out duration-500"
+//                 />
+//               </div>
+//               <div className="flex-grow">
+//                 <h3 className="mt-2 font-bold text-center">{item.title}</h3>
+//                 <p className="mt-1 text-gray-700 text-center">${item.price}</p>
+//               </div>
+//             </div>
+//           </Link>
+//         ))}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ProductData
+
+
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import FilteredData from './FilteredData'
 
 const ProductData = () => {
-    const [products, setProducts] = useState([])
-    const [query, setQuery] = useSearchParams()
+  const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
+  const [query, setQuery] = useSearchParams()
 
-    const  getData = async() => {
-        let res = await axios.get('https://fakestoreapi.com/products')
-        setProducts(res.data)
-        console.log(res.data)
+  const getData = async () => {
+    let res = await axios.get('https://fakestoreapi.com/products')
+    setProducts(res.data)
+    setFilteredProducts(res.data) 
+    console.log(res.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  useEffect(() => {
+    let sort = query.get('sort')
+    let category = query.get('category')
+    let search = query.get('search')
+    console.log(sort, category, search)
+
+    let filtered = [...products]
+
+    if (category) {
+      filtered = filtered.filter((item) => item.category === category)
     }
-    useEffect(()=>{
-        getData()
-    },[])
 
-    useEffect(()=>{
-        let sort = query.get('sort')
-        let category = query.get('category')
-        console.log(sort, category)
+    if (search) {
+      filtered = filtered.filter((item) =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      )
+    }
 
-        if(sort ==="LTH"){
-          let data = [...products]
-          data = data.sort((a, b) => a.price - b.price)
-          console.log(data)
-          setProducts(data)
-        }
-        else{
-            let data = [...products]
-            data = data.sort((a, b) => b.price - a.price)
-            console.log(data)
-            setProducts(data)
-            
-        }
-        if(category){
-            let data = [...products]
-            data = data.filter((item)=>item.category == category)
-            console.log(data)
-            setProducts(data)
-        }
-    },[query])
+    if (sort === "LTH") {
+      filtered = filtered.sort((a, b) => a.price - b.price)
+    }
+    else {
+      filtered = filtered.sort((a, b) => b.price - a.price)
+    }
+
+    setFilteredProducts(filtered)
+  }, [query, products])
+
   return (
     <div>
-        <FilteredData/>
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 shadow gap-9'>
-        {products.map((ele)=>(
-            <div key={ele.id}>
-                <img src={ele.image} alt={ele.title} />
-                <h3>{ele.title}</h3>
-                <p>{ele.description}</p>
+      <FilteredData />
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 shadow gap-5 mt-5'>
+        {filteredProducts.map((item) => (
+          <Link key={item.id} to={`/product/${item.id}`}>
+            <div className="border p-4 rounded-lg shadow transition-transform duration-500 ease-in-out hover:shadow-lg hover:scale-105 flex flex-col h-full">
+              <div className="flex justify-center items-center h-32 flex-shrink-0">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="max-h-full max-w-full object-contain hover:scale-105 ease-in-out duration-500"
+                />
+              </div>
+              <div className="flex-grow">
+                <h3 className="mt-2 font-bold text-center">{item.title}</h3>
+                <p className="mt-1 text-gray-700 text-center"> Price : $ {item.price}</p>
+              </div>
             </div>
+          </Link>
         ))}
-    </div>
+      </div>
     </div>
   )
 }
 
 export default ProductData
+
