4e58659d64cc0201d81fc38dea1c596b8906dc82
ShaileshVSavani <svs.rnw23@gmail.com>
2024-08-12 18:24:16 +0530

    1

--- router1/pages/Signup.jsx
+++ router1/pages/Signup.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 const Signup = () => {
   return (
-    <div>
+    <div >
       <h1>This is Signup Page.......</h1>
-    </div>
-  )
-}
+      <div className="bg-dark text-white p-5 rounded-3 ">
+      <Form className="w-50 mx-auto bg-gray">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-export default Signup
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
--- router1/src/App.jsx
+++ router1/src/App.jsx
@@ -1,32 +1,17 @@
 
-import "./App.css";
-import AllRoutes from "../Components/AllRoutes";
-import Navb from "../Components/Nav";
-import { Route, Routes } from "react-router-dom";
-import Home from "../pages/Home";
-import Product from "../pages/Product";
-import Signup from "../pages/Signup";
-import Login from "../pages/Login";
-import NotFound from "../pages/NotFound";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import AllRoutes from '../Components/AllRoutes'
+import Navb from '../Components/Nav'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
-
   return (
     <>
-      <Navb />
-      {/* <AllRoutes/> */}
-      <Routes>
-           <Route path="/" element={<Home/> }/>
-           <Route path="/product" element={<Product/>} />
-           <Route path="/signup" element={<Signup/>} />
-           <Route path="/login" element={<Login/>} />
-           <Route path="*" element={<NotFound/>} />
-        </Routes>
-
+      <Navb/>
+      <AllRoutes/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
